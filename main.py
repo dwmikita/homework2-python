23e3fbdf8815e87191d795b3319b4da72ec3b253
Daniel Mikita <djm6907@psu.edu>
2020-09-11 22:55:42 +0000

    formatting

--- main.py
+++ main.py
@@ -21,21 +21,21 @@ def getGradePoint(grade):
     return 0.0 
 
 def run():
-  letter1 = input("Enter your course 1 grade: ")
+  letter1 = input("Enter your course 1 letter grade: ")
   credit1 = input("Enter your course 1 credits: ")
   credit1 = float(credit1)
   print(f"Grade point for course 1 is: {getGradePoint(letter1)}.")
 
-  letter2 = input("Enter your course 1 grade: ")
-  credit2 = input("Enter your course 1 credits: ")
+  letter2 = input("Enter your course 2 letter grade: ")
+  credit2 = input("Enter your course 2 credits: ")
   credit2 = float(credit2)
-  print(f"Grade point for course 1 is: {getGradePoint(letter2)}.")
+  print(f"Grade point for course 2 is: {getGradePoint(letter2)}.")
   
 
-  letter3 = input("Enter your course 1 grade: ")
-  credit3 = input("Enter your course 1 credits: ")
+  letter3 = input("Enter your course 3 letter grade:  ")
+  credit3 = input("Enter your course 3 credits: ")
   credit3 = float(credit3)
-  print(f"Grade point for course 1 is:  {getGradePoint(letter3)}.")
+  print(f"Grade point for course 3 is:  {getGradePoint(letter3)}.")
   
   GPA = (getGradePoint(letter1) * credit1 + getGradePoint(letter2) * credit2 + getGradePoint(letter3) * credit3) / (credit1 + credit2 + credit3) 
   print(f"Your GPA is: {GPA}")
