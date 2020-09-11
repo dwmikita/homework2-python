3fcc8d31a4ea9d5bc4a20d4a2e1d537ce46bdda4
Daniel Mikita <djm6907@psu.edu>
2020-09-11 22:49:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# Author: Daniel Mikita djm6907@psu.edu
+
+def getGradePoint(grade):
+  if (grade == 'A'):
+    return 4.0
+  elif (grade == 'A-'):
+    return 3.67
+  elif (grade == 'B+'):
+    return 3.33
+  elif (grade == 'B'):
+    return 3.0
+  elif (grade == 'B-'):
+    return 2.67
+  elif (grade == 'C+'):
+    return 2.33
+  elif (grade == 'C'):
+    return 2.0
+  elif (grade == 'D'):
+    return 1.0
+  else:
+    return 0.0
+
+def run():
+  letter1 = input("Enter your course 1 grade: ")
+  credit1 = input("Enter your course 1 credits: ")
+  credit1 = float(credit1)
+  print(f"Your letter grade for course 1 is {getGradePoint(letter1)}.")
+
+  letter2 = input("Enter your course 1 grade: ")
+  credit2 = input("Enter your course 1 credits: ")
+  credit2 = float(credit2)
+  print(f"Your letter grade for course 1 is {getGradePoint(letter2)}.")
+  
+
+  letter3 = input("Enter your course 1 grade: ")
+  credit3 = input("Enter your course 1 credits: ")
+  credit3 = float(credit3)
+  print(f"Your letter grade for course 1 is {getGradePoint(letter3)}.")
+  
+  GPA = (getGradePoint(letter1) * credit1 + getGradePoint(letter2) * credit2 + getGradePoint(letter3) * credit3) / (credit1 + credit2 + credit3) 
+  print(f"Your GPA is: {GPA}")
+  
+
+if __name__== "__main__" :
+  run()
