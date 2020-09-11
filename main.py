9abb7187f6161366bc5cfa7e41687f660b7726c7
Daniel Mikita <djm6907@psu.edu>
2020-09-11 22:53:22 +0000

    Wording hehe

--- main.py
+++ main.py
@@ -24,18 +24,18 @@ def run():
   letter1 = input("Enter your course 1 grade: ")
   credit1 = input("Enter your course 1 credits: ")
   credit1 = float(credit1)
-  print(f"Your letter grade for course 1 is {getGradePoint(letter1)}.")
+  print(f"Grade point for course 1 is: {getGradePoint(letter1)}.")
 
   letter2 = input("Enter your course 1 grade: ")
   credit2 = input("Enter your course 1 credits: ")
   credit2 = float(credit2)
-  print(f"Your letter grade for course 1 is {getGradePoint(letter2)}.")
+  print(f"Grade point for course 1 is: {getGradePoint(letter2)}.")
   
 
   letter3 = input("Enter your course 1 grade: ")
   credit3 = input("Enter your course 1 credits: ")
   credit3 = float(credit3)
-  print(f"Your letter grade for course 1 is {getGradePoint(letter3)}.")
+  print(f"Grade point for course 1 is:  {getGradePoint(letter3)}.")
   
   GPA = (getGradePoint(letter1) * credit1 + getGradePoint(letter2) * credit2 + getGradePoint(letter3) * credit3) / (credit1 + credit2 + credit3) 
   print(f"Your GPA is: {GPA}")
